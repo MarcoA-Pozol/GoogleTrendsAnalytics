542ddfd904433fc81c6475534e04d581c6d201f3
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-05-06 09:46:26 -0600

    Rename and load datasets.

---
+++ GetData/mexico_trends_dataset.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv('./Datasets/mx_week18_trends.csv')
+
+print(df)
---
+++ GetData/usa_trends_dataset.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv('./Datasets/usa_week18_trends.csv')
+
+print(df)
