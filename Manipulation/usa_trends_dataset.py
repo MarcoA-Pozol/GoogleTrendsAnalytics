37682c6fc665b56418b8ad8a6f5c2bba7e1dfda9
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-05-06 13:20:14 -0600

    Top 10 most and less popular trends in USA using SQL queries over a dataframe.

--- Manipulation/usa_trends_dataset.py
+++ Manipulation/usa_trends_dataset.py
@@ -1,7 +1,25 @@
 import pandas as pd
+import sqlite3
 from Utils.get_dataset_chunk import get_dataset_chunk
+
+conn = sqlite3.connect('Trends2025.db')
 
 df = pd.read_csv('./Datasets/usa_week18_trends.csv')
 
 # Get dataset chunk
-print(get_dataset_chunk(filepath='./Datasets/usa_week18_trends.csv', chunknumber=4, chunksize=100, viewallrows=True, viewallcolumns=False))
+df = get_dataset_chunk(filepath='./Datasets/usa_week18_trends.csv', chunknumber=4, chunksize=100, viewallrows=False, viewallcolumns=True)
+
+# Save dataframe as sql table
+df.to_sql('UsaTrends', conn, if_exists='replace', index=False)
+
+# Top 10 most popular trends by search volume in USA
+query = 'SELECT Trends, "Search volume" FROM UsaTrends ORDER BY "Search volume" DESC LIMIT 10;'
+result = pd.read_sql(query, conn)
+
+print(result)
+
+
+# Top 10 less popular trends by search volume in USA
+query = 'SELECT Trends, "Search volume" from UsaTrends ORDER BY "Search volume" ASC LIMIT 10;'
+result = pd.read_sql(query, conn)
+print(result)
